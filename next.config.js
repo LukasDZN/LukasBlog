bbb9811daa9a64ad04d929b63b20c5a8b734241f
LukasDZN <Dzenk.Lukas@gmail.com>
2022-10-29 04:03:23 +0300

    Analytics bugifx

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app app.posthog;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src giscus.app;
 `
 
 const securityHeaders = [
@@ -73,7 +73,10 @@ module.exports = () => {
         },
       ]
     },
-    webpack: (config, options) => {
+    webpack: (
+      config
+      // options
+    ) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
