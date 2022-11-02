87027f4e132e3f9df8fe2c2451a58fdc7cb6df12
LukasDZN <Dzenk.Lukas@gmail.com>
2022-11-02 06:15:11 +0200

    Added this website as a project item

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,28 @@
 const projectsData = [
   {
     title: 'Markdown header sorter',
-    description: `Copy and paste your text into the editor tool to sort your markdown sections alphabetically. Sort your entire text or select a single header level (#, ##, ...).`,
+    description: `Copy and paste your text into the editor tool to sort your markdown sections alphabetically. Sort your entire text or select a single header level (#, ##, ...).
+    [Vanilla JS]`,
     imgSrc: '/static/images/markdown-header-sorter-logo-android-chrome-512x512.png',
     href: 'https://lukasdzn.github.io/Markdown-header-sorter/',
   },
   {
     title: 'Markdown internal link checker',
-    description: `A Node JS script that parses .md files within "input" directory and prints out an array of broken URLs that were found.`,
-    imgSrc: '/static/images/internal-link-checker-logo-large1.png',
+    description: `A script that parses .md files within "input" directory and prints out an array of broken URLs that were found.
+    [TypeScript, Node JS]`,
+    imgSrc: '/static/images/internal-link-checker-logo-large.png',
     href: 'https://github.com/LukasDZN/markdownUrlChecker',
   },
   {
+    title: 'This blog',
+    description: `[TypeScript, Next JS]`,
+    imgSrc: '/static/images/website-logo-and-title-large.png',
+    href: 'https://github.com/LukasDZN/LukasBlog',
+  },
+  {
     title: 'Redmine task notifications',
-    description: `Chrome extension for creating Redmine (project management system) status alerts (Vanilla JS).`,
+    description: `Chrome extension for creating Redmine (project management system) status alerts.
+    [Vanilla JS]`,
     imgSrc: '/static/images/work-in-progress-logo-large.png',
     href: 'https://github.com/LukasDZN/redmineTaskAlertChromeExtension',
   },
